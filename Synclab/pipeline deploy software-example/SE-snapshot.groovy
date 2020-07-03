7c4aa4beeaecc0f7c42e6816ce817492bf89cda8
e5471 <soledadnatalia.kuczala@igt.com>
2020-07-03 11:59:42 +0200

    updated pipeline

---
+++ Synclab/pipeline deploy software-example/SE-snapshot.groovy
@@ -0,0 +1,35 @@
+pipeline {
+      options { timestamps () }
+      agent {label 'master'}
+
+      stages{
+          stage('Clean Workspace'){
+              steps{
+                  script {
+                    deleteDir()
+                  }
+                }
+          }
+        stage('Nexus checkout') {
+              steps {
+                    script {
+                      sh """
+                        curl -O http://nexus.synclab:8081/repository/icon-evo-fe-snapshots/@icon-evo/application/application-${VERSION}.tar.gz 
+                        tar -xvf *tar.gz
+                        rm *tar.gz 
+                      """
+                    }
+                }
+        }
+        stage('Deploy Docker Agent'){
+               steps {
+                    script {
+                    sh """
+                        ssh deploymng@newicontest.synclab "rm -rf /var/www/html/*"
+                        scp *.* deploymng@newicontest.synclab:/var/www/html
+                    """
+                }
+            }
+        }
+    }
+}
